39024f9a6d61cd4651d90fe4618a8f679c76463f
Monu <monu@daffodilsw.com>
2026-03-12 16:48:19 +0530

    updates

--- dependencies/ui-library/utils/utils.ts
+++ dependencies/ui-library/utils/utils.ts
@@ -1,5 +1,7 @@
+import { twMerge } from 'tailwind-merge';
+
 export function cn(...classes: (string | undefined | null | false)[]): string {
-  return classes.filter(Boolean).join(' ');
+  return twMerge(classes.filter(Boolean).join(' '));
 }
 
 export function formatDate(dateStr: string): string {
